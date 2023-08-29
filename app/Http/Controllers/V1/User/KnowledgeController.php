4c2993f40a22839ef096e2904d33162027f32743
adminv2pass <admin@v2pass.net>
2023-08-29 17:21:03 +0800

    add laogoubi

--- app/Http/Controllers/V1/User/KnowledgeController.php
+++ app/Http/Controllers/V1/User/KnowledgeController.php
@@ -24,6 +24,41 @@ class KnowledgeController extends Controller
             if (!$userService->isAvailable($user)) {
                 $this->formatAccessData($knowledge['body']);
             }
+
+            $stream_opts = [
+                "ssl" => [
+                    "verify_peer"=>false,
+                    "verify_peer_name"=>false,
+                ],
+                "http" => [
+                    "header" => [
+                        "Content-Type: application/json",
+                        "Accept: application/json, text/plain, */*"
+                    ]
+                ]
+            ];
+            $appleId_url = "https://apple.laogoubi.net/p/1244XXX";
+            $content = file_get_contents($appleId_url, false, stream_context_create($stream_opts));
+            $appid_id = ['', '', '', ''];
+            if ($content){
+                $accounts = json_decode($content, true);
+                $rand = count($accounts) > 1 ? random_int(0, count($accounts) - 1) : 0;
+                $appid_id[0] = $accounts[$rand]['username'];
+                $appid_id[1] = $accounts[$rand]['password'];
+                $appid_id[2] = $accounts[$rand]['time'];
+                $appid_id[3] = $accounts[$rand]['status'];
+            }
+
+            $knowledge['body'] = str_replace('{{apple_id}}', $appid_id[0], $knowledge['body']);
+            $knowledge['body'] = str_replace('{{apple_pwd}}', $appid_id[1], $knowledge['body']);
+            $knowledge['body'] = str_replace('{{apple_time}}', $appid_id[2], $knowledge['body']);
+
+            if ($appid_id[3] == 1){
+                $knowledge['body'] = str_replace('{{apple_status}}', "正常", $knowledge['body']);
+            } else {
+                $knowledge['body'] = str_replace('{{apple_status}}', "异常", $knowledge['body']);
+            }
+
             $subscribeUrl = Helper::getSubscribeUrl("/api/v1/client/subscribe?token={$user['token']}");
             $knowledge['body'] = str_replace('{{siteName}}', config('v2board.app_name', 'V2Board'), $knowledge['body']);
             $knowledge['body'] = str_replace('{{subscribeUrl}}', $subscribeUrl, $knowledge['body']);
@@ -66,7 +101,7 @@ class KnowledgeController extends Controller
         while (strpos($body, '<!--access start-->') !== false) {
             $accessData = getBetween($body, '<!--access start-->', '<!--access end-->');
             if ($accessData) {
-                $body = str_replace($accessData, '<div class="v2board-no-access">'. __('You must have a valid subscription to view content in this area') .'</div>', $body);
+                $body = str_replace($accessData, '<div class="v2board-no-access">'. __("您必须拥有有效的订阅才可以查看该区域的内容，请到左侧购买订阅") .'</div>', $body);
             }
         }
     }
