7bb5536f713bcc670ded028843cb7d91f5df1c5b
adminv2pass <admin@v2pass.net>
2023-09-12 14:29:09 +0800

    fix: epusdt payment

--- app/Payments/EPUSDT.php
+++ app/Payments/EPUSDT.php
@@ -2,9 +2,13 @@
 
 namespace App\Payments;
 
-use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redirect;
 
 class EPUSDT {
+
+    private $config;
+
     public function __construct($config)
     {
         $this->config = $config;
@@ -15,27 +19,17 @@ class EPUSDT {
         return [
             'url' => [
                 'label' => 'URL',
-                'description' => '',
+                'description' => 'epusdt收银台地址+/api/v1/order/create-transaction 如：http://127.0.0.1:8000/api/v1/order/create-transaction',
                 'type' => 'input',
             ],
             'pid' => [
                 'label' => 'PID',
-                'description' => '',
-                'type' => 'input',
-            ],
-            'key' => [
-                'label' => 'KEY',
-                'description' => '',
-                'type' => 'input',
-            ],
-            'type' => [
-                'label' => 'TYPE',
-                'description' => '',
+                'description' => 'api接口认证token',
                 'type' => 'input',
             ],
             'epusdt_pay_channel' => [
                 'label' => 'Channel',
-                'description' => '您的 EpusdtPay 支付通道(例如: trc20, polygon)',
+                'description' => '您的 epusdt 支付通道(例如: trc20, polygon)',
                 'type' => 'input',
             ]
         ];
@@ -51,36 +45,17 @@ class EPUSDT {
             'channel' => $this->config['epusdt_pay_channel'],
         ];
         $params['signature'] = $this->epusdtSign($params, $this->config['pid']);
-        $client = new Client([
-            'headers' => [ 'Content-Type' => 'application/json' ]
-        ]);
-        $response = $client->post($this->config['url'], ['body' => json_encode($params)]);
-        $body = json_decode($response->getBody()->getContents(), true);
+
+        $response = Http::withHeaders(['Content-Type' => 'application/json'])
+            ->post($this->config['url'], $params);
+        $body = $response->json();
         if (!isset($body['status_code']) || $body['status_code'] != 200) {
-            abort(500, __("请求失败") . $body['message']);
+            throw new \Illuminate\Http\Exceptions\HttpResponseException(response(__("请求失败") . $body['message'], 500));
         }
-        return redirect()->away($body['data']['payment_url']);
-
+        return Redirect::away($body['data']['payment_url']);
     }
 
-    public function notify($params)
-    {
-        $sign = $params['sign'];
-        unset($params['sign']);
-        unset($params['sign_type']);
-        ksort($params);
-        reset($params);
-        $md5 = $this->epusdtSign($params, $this->config['pid']);
-        if ($sign !== $md5) {
-            return false;
-        }
-        return [
-            'trade_no' => $params['out_trade_no'],
-            'callback_no' => $params['trade_no']
-        ];
-    }
-
-    function epusdtSign(array $parameter, string $signKey)
+    private function epusdtSign(array $parameter, string $signKey)
     {
         ksort($parameter);
         reset($parameter); //内部指针指向数组中的第一个元素
@@ -94,9 +69,24 @@ class EPUSDT {
                     $urls .= "&";
                 }
                 $sign .= "$key=$val"; //拼接为url参数形式
-                $urls .= "$key=" . urlencode($val); //拼接为url参数形式
             }
         }
         return md5($sign . $signKey); //密码追加进入开始MD5签名
+    }
+
+    public function notify($params)
+    {
+        $status = $params['status'];
+        // 1：等待支付，2：支付成功，3：已过期
+        if ($status != 2) {
+            return 'fail';
+        }
+        $sign = $params['signature'];
+        unset($params['signature']);
+        $md5 = $this->epusdtSign($params, $this->config['pid']);
+        if ($sign !== $md5) {
+            return 'fail';
+        }
+        return 'ok';
     }
 }
