0b6e436629067fa5b5f7aa66719c10b1669e1ce1
adminv2pass <admin@v2pass.net>
2023-09-20 17:40:39 +0800

    fix: sing-box subscription

--- app/Protocols/SingBox.php
+++ app/Protocols/SingBox.php
@@ -260,7 +260,6 @@ class SingBox
         $array['up_mbps'] = $server['up_mbps'];
         $array['down_mbps'] = $server['down_mbps'];
         $array['obfs'] = Helper::getServerKey($server['created_at'], 16);
-        $array['tls'] =[];
 
         if (!empty($server['server_name'])) $array['tls']['server_name'] = $server['server_name'];
         if (!empty($server['insecure'])) $array['tls']['insecure'] = ($server['insecure'] ? true : false);
