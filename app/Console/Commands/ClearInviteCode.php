55f7f4be19daec7d30f4fdc33c1c45dbaaa10727
adminv2pass <admin@v2pass.net>
2023-09-28 14:55:19 +0800

    fix: invite code auto delete

--- app/Console/Commands/ClearInviteCode.php
+++ app/Console/Commands/ClearInviteCode.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\InviteCode;
+use App\Services\TelegramService;
 use App\Utils\Helper;
 use Illuminate\Console\Command;
 use App\Models\User;
@@ -43,8 +44,9 @@ class ClearInviteCode extends Command
     public function handle()
     {
         ini_set('memory_limit', -1);
+        $telegramService = new TelegramService();
         $this->info("功能：删除过期7天以上的【按周期付费用户】的邀请码 以及 7天以上不使用流量的【一次性套餐用户】的邀请码");
-
+        $telegramService->sendMessageWithAdmin("开始删除特定用户的邀请码，指：过期 7 天以上的【按周期付费用户】和 7 天以上不使用流量的【一次性套餐用户】");
         $daysToExpire = 7;
         $daysWithoutTraffic = 7;
 
@@ -52,6 +54,7 @@ class ClearInviteCode extends Command
         $plans = Plan::all();
         $oneTimePlans = [];
         $trafficPlans = [];
+        $counts =0;
         foreach ($plans as $plan) {
             if ($plan->onetime_price !== null && $plan->onetime_price !== 0) {
                 $oneTimePlans[] = $plan->id;
@@ -66,6 +69,7 @@ class ClearInviteCode extends Command
             if ((in_array($user->plan_id, $oneTimePlans) && (time() - $user->updated_at) > ($daysWithoutTraffic * 86400)) ||
                 (in_array($user->plan_id, $trafficPlans) && (time() - $user->expired_at) > ($daysToExpire * 86400))) {
                 if (InviteCode::where('user_id', $user->id)->delete()) {
+                    $counts++;
                     $this->info("已删除用户ID为{$user->id}的邀请码");
                 }
             } else {
@@ -73,5 +77,6 @@ class ClearInviteCode extends Command
             }
         }
 
+        $telegramService->sendMessageWithAdmin("每日例行检查用户有效性，删除了{$counts}个邀请码");
     }
 }
