d85ff6db5b79e6ad557c88ed277e1078905f1858
adminv2pass <admin@v2pass.net>
2023-09-28 14:14:22 +0800

    fix: invite code auto delete

--- app/Console/Commands/ClearInviteCode.php
+++ app/Console/Commands/ClearInviteCode.php
@@ -6,6 +6,7 @@ use App\Models\InviteCode;
 use App\Utils\Helper;
 use Illuminate\Console\Command;
 use App\Models\User;
+use App\Models\Plan;
 
 class ClearInviteCode extends Command
 {
@@ -44,20 +45,30 @@ class ClearInviteCode extends Command
         ini_set('memory_limit', -1);
         $this->info("功能：删除过期7天以上的按周期付费用户的邀请码 以及 5天以上不使用流量的一次性套餐用户的邀请码");
         $users = User::all();
-        foreach ($users as $user)
-        {
-            if(($user->plan_id <= 5 || $user->plan_id == 11 || $user->plan_id == 12 || $user->plan_id == 14) && (time() - $user->expired_at) > 604800){
-                if (InviteCode::where('user_id', $user->id)->delete()) {
-                    $this->info("已删除用户ID为{$user->id}的邀请码");
-                }
+        $plans = Plan::all();
+        $timePlans = [];
+        foreach ($plans as $plan) {
+            if ($plan->onetime_price !== null && $plan->onetime_price !== 0) {
+                $timePlans[] = $plan->id;
             }
-
-            if(($user->plan_id == 6 or $user->plan_id == 7 or $user->plan_id == 13) && (time() - $user->updated_at) > 432000){
-                if (InviteCode::where('user_id', $user->id)->delete()) {
-                    $this->info("已删除用户(长期套餐)ID为{$user->id}的邀请码");
-                }
-            }
-
         }
+
+        foreach ($users as $user) {
+            if (in_array($user->plan_id, $timePlans) && (time() - $user->updated_at) > 432000) {
+                // Delete invitation code for users with plan_id in $timePlans
+                // Delete invitation code for users who haven't used traffic for 5 days
+                if (InviteCode::where('user_id', $user->id)->delete()) {
+                    $this->info("已删除用户(拥有长期套餐)ID为{$user->id}的邀请码");
+                }
+            } else {
+                // Delete invitation code for users who have been expired for 7 days or more
+                if((time() - $user->expired_at) > 604800){
+                    if (InviteCode::where('user_id', $user->id)->delete()) {
+                        $this->info("已删除用户ID为{$user->id}的邀请码");
+                    }
+                }
+            }
+        }
+
     }
 }
