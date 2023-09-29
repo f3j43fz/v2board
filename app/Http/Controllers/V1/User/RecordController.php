284b8f78e438c3c74931ca429031589eb4a41ac4
adminv2pass <admin@v2pass.net>
2023-09-29 22:54:22 +0800

    ip mask

--- app/Http/Controllers/V1/User/RecordController.php
+++ app/Http/Controllers/V1/User/RecordController.php
@@ -36,27 +36,67 @@ class RecordController extends Controller
         ]);
     }
 
+    private function expandIPv6Address($ip)
+    {
+        // Check if the input is a valid IPv6 address
+        if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+            return false;
+        }
+
+        // Split the IPv6 address into groups
+        $groups = explode(':', $ip);
+
+        // Count the number of groups
+        $groupCount = count($groups);
+
+        // Find the index of the compressed group (::)
+        $compressedGroupIndex = array_search('', $groups);
+
+        // Calculate the number of groups needed to expand the address
+        $expandedGroupCount = 8 - $groupCount;
+
+        // Expand the compressed group (::) with zeros
+        if ($compressedGroupIndex !== false) {
+            $expandedGroups = array_merge(
+                array_slice($groups, 0, $compressedGroupIndex),
+                array_fill(0, $expandedGroupCount, '0000'),
+                array_slice($groups, $compressedGroupIndex + 1)
+            );
+        } else {
+            $expandedGroups = $groups;
+        }
+
+        // Pad each group with zeros to ensure four hexadecimal digits
+        $expandedGroups = array_map(function ($group) {
+            return str_pad($group, 4, '0', STR_PAD_LEFT);
+        }, $expandedGroups);
+
+        // Join the expanded groups and format the IPv6 address
+        $expandedIp = implode(':', $expandedGroups);
+
+        return $expandedIp;
+    }
+
     private function maskIpAddress($ipAddress)
     {
         // Check if the IP address is IPv4
         if (filter_var($ipAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
             $parts = explode('.', $ipAddress);
+            $parts[1] = '*';
             $parts[2] = '*';
-            $parts[3] = '*';
             return implode('.', $parts);
         }
 
-        // Check if the IP address is IPv6
-        if (filter_var($ipAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-            $parts = explode(':', $ipAddress);
-            $parts[count($parts) - 2] = '*';
-            $parts[count($parts) - 1] = '*';
-            return implode(':', $parts);
-        }
-
-        // Return the original IP address if it's not valid
-        return $ipAddress;
+        // expand ipv6
+        $ipv6 = $this->expandIPv6Address($ipAddress);
+        $parts = explode(':', $ipv6);
+        $parts[count($parts) - 4] = '*';
+        $parts[count($parts) - 3] = '*';
+        $parts[count($parts) - 2] = '*';
+        $parts[count($parts) - 1] = '*';
+        return implode(':', $parts);
     }
+
 
 
 }
