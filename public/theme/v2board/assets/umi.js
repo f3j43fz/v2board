ce425db2eb556f598d714a155daf0e841a454c03
adminv2pass <admin@v2pass.net>
2023-09-29 14:00:41 +0800

    fix: info

--- public/theme/v2board/assets/umi.js
+++ public/theme/v2board/assets/umi.js
@@ -26596,17 +26596,17 @@
                                         scroll: {
                                             x: 800
                                         }
-                                    }))), m.a.createElement("div", {
+                                    }))), l.a.createElement("div", {
                                 className: "block block-content block-content-full"
-                            }, m.a.createElement("div", {
+                            }, l.a.createElement("div", {
                                 className: "font-w400"
                             }, Object(w["formatMessage"])({
                                 id: "sub_request_info1"
-                            })), m.a.createElement("div", {
+                            })), l.a.createElement("div", {
                                 className: "font-w400"
                             }, Object(w["formatMessage"])({
                                 id: "sub_request_info2"
-                            })), m.a.createElement("div", {
+                            })), l.a.createElement("div", {
                                 className: "font-w600"
                             }, Object(w["formatMessage"])({
                                 id: "sub_request_info3"
