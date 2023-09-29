70afc688c827aabb0c94fad1f5927704159dab2c
adminv2pass <admin@v2pass.net>
2023-09-29 17:28:33 +0800

    add safety

--- app/Http/Controllers/V1/Client/ClientController.php
+++ app/Http/Controllers/V1/Client/ClientController.php
@@ -22,15 +22,17 @@ class ClientController extends Controller
         // 过滤无效用户
         // 过滤无效用户
         // 过滤无效用户
+        // 过滤无效用户
         if (!$userService->isAvailable($user)){
             header('HTTP/1.1 403 Forbidden');
             header('Content-Type: application/json; charset=utf-8');
             $response = [
                 'error' => '您已被 Ban 或者套餐已过期'
             ];
-            echo json_encode($response, JSON_UNESCAPED_UNICODE);
+            echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
             exit();
         }
+
 
 
 
