e7bf5d51dc3dbe7b2f225d763978f9df075c0199
adminv2pass <admin@v2pass.net>
2023-12-02 12:48:39 +0800

    update

--- app/Plugins/Telegram/Commands/ReplyTicket.php
+++ app/Plugins/Telegram/Commands/ReplyTicket.php
@@ -24,10 +24,12 @@ class ReplyTicket extends Telegram {
         }
         if (!$msg->text) return;
         if (!($user->is_admin || $user->is_staff)) return;
-
+        $telegramService = $this->telegramService;
         if ($msg->text === 'invite') {
-            $telegramService = $this->telegramService;
-            $msg->text = ($user->telegram_id > 0) ? $telegramService->createChatInviteLink(config('v2board.telegram_group_id')) . " 请复制该链接，粘贴到浏览器，即可加入群聊。  注意事项：  1. 进群前请设置用户名，否则会被封禁；  2. 进群后请回答一个简单的数学问题，不要瞎回答，否则会被封禁；  3.邀请链接时效性为5分钟，超时后无法加入" : "您还没有绑定我们的机器人，请先到官网左侧的【个人中心】，绑定您的 Telegram 账号。";
+            $ticket = Ticket::where('id', $ticketId)
+                ->first();
+            $telegram_id = User::where('id', $ticket->user_id)->value('telegram_id');
+            $msg->text = ($telegram_id > 0) ? $telegramService->createChatInviteLink(config('v2board.telegram_group_id')) . " 请复制该链接，粘贴到浏览器，即可加入群聊。  注意事项：  1. 进群前请设置用户名，否则会被封禁；  2. 进群后请回答一个简单的数学问题，不要瞎回答，否则会被封禁；  3.邀请链接时效性为5分钟，超时后无法加入" : "您还没有绑定我们的机器人，请先到官网左侧的【个人中心】，绑定您的 Telegram 账号。";
         }
         $ticketService = new TicketService();
         $ticketService->replyByAdmin(
@@ -35,7 +37,6 @@ class ReplyTicket extends Telegram {
             $msg->text,
             $user->id
         );
-        $telegramService = $this->telegramService;
         $telegramService->sendMessage($msg->chat_id, "#`{$ticketId}` 的工单已回复成功", 'markdown');
         $telegramService->sendMessageWithAdmin("#`{$ticketId}` 的工单已由 {$user->email} 进行回复", true);
     }
