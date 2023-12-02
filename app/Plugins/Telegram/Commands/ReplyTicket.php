597bc21dc36e7ba657bb278f3ddec2fa754dfdb2
adminv2pass <admin@v2pass.net>
2023-12-02 11:43:35 +0800

    update

--- app/Plugins/Telegram/Commands/ReplyTicket.php
+++ app/Plugins/Telegram/Commands/ReplyTicket.php
@@ -24,8 +24,8 @@ class ReplyTicket extends Telegram {
         }
         if (!$msg->text) return;
         if (!($user->is_admin || $user->is_staff)) return;
+        $telegramService = $this->telegramService;
         if ($msg->text == 'invite'){
-            $telegramService = $this->telegramService;
             $msg->text = $telegramService->createChatInviteLink(config('v2board.telegram_group_id')) . '  ' . '请复制链接，粘贴到浏览器，即可加入群聊。  注意事项：  1. 进群前请设置用户名，否则会被封禁；  2. 进群后请回答一个简单的数学问题，不要瞎回答，否则会被封禁；  3.邀请链接时效性为5分钟，超时后无法加入';
         }
         $ticketService = new TicketService();
@@ -34,7 +34,6 @@ class ReplyTicket extends Telegram {
             $msg->text,
             $user->id
         );
-        $telegramService = $this->telegramService;
         $telegramService->sendMessage($msg->chat_id, "#`{$ticketId}` 的工单已回复成功", 'markdown');
         $telegramService->sendMessageWithAdmin("#`{$ticketId}` 的工单已由 {$user->email} 进行回复", true);
     }
