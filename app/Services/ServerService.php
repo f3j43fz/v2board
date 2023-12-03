d7cdf6922e383d24534195c8a72e29183ea97f7b
adminv2pass <admin@v2pass.net>
2023-12-03 21:48:59 +0800

    update

--- app/Services/ServerService.php
+++ app/Services/ServerService.php
@@ -36,7 +36,7 @@ class ServerService
 
             // delete private_key
             if (isset($server[$key]['tls']) && (int)$server[$key]['tls'] === 2) {
-                unset($server[$key]['tls_settings']['private_key']);
+                $server[$key]['tls_settings'] = array_diff_key($server[$key]['tls_settings'],['private_key']);
             }
 
             $servers[] = $server[$key]->toArray();
