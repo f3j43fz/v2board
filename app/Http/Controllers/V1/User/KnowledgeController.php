d232a4f1a013eae4a55cf1e02fde60211044c6c5
adminv2pass <admin@v2pass.net>
2023-12-04 16:00:20 +0800

    update kno

--- app/Http/Controllers/V1/User/KnowledgeController.php
+++ app/Http/Controllers/V1/User/KnowledgeController.php
@@ -24,41 +24,6 @@ class KnowledgeController extends Controller
             if (!$userService->isAvailable($user)) {
                 $this->formatAccessData($knowledge['body']);
             }
-
-            $stream_opts = [
-                "ssl" => [
-                    "verify_peer"=>false,
-                    "verify_peer_name"=>false,
-                ],
-                "http" => [
-                    "header" => [
-                        "Content-Type: application/json",
-                        "Accept: application/json, text/plain, */*"
-                    ]
-                ]
-            ];
-            $appleId_url = "https://apple.laogoubi.net/p/1244XXX";
-            $content = file_get_contents($appleId_url, false, stream_context_create($stream_opts));
-            $appid_id = ['', '', '', ''];
-            if ($content){
-                $accounts = json_decode($content, true);
-                $rand = count($accounts) > 1 ? random_int(0, count($accounts) - 1) : 0;
-                $appid_id[0] = $accounts[$rand]['username'];
-                $appid_id[1] = $accounts[$rand]['password'];
-                $appid_id[2] = $accounts[$rand]['time'];
-                $appid_id[3] = $accounts[$rand]['status'];
-            }
-
-            $knowledge['body'] = str_replace('{{apple_id}}', $appid_id[0], $knowledge['body']);
-            $knowledge['body'] = str_replace('{{apple_pwd}}', $appid_id[1], $knowledge['body']);
-            $knowledge['body'] = str_replace('{{apple_time}}', $appid_id[2], $knowledge['body']);
-
-            if ($appid_id[3] == 1){
-                $knowledge['body'] = str_replace('{{apple_status}}', "正常", $knowledge['body']);
-            } else {
-                $knowledge['body'] = str_replace('{{apple_status}}', "异常", $knowledge['body']);
-            }
-
             $subscribeUrl = Helper::getSubscribeUrl("/api/v1/client/subscribe?token={$user['token']}");
             $knowledge['body'] = str_replace('{{siteName}}', config('v2board.app_name', 'V2Board'), $knowledge['body']);
             $knowledge['body'] = str_replace('{{subscribeUrl}}', $subscribeUrl, $knowledge['body']);
