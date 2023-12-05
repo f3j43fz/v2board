5f89aca19efdb790d39bc47e8a5204534ac70b17
adminv2pass <admin@v2pass.net>
2023-12-05 15:20:49 +0800

    update info

--- app/Protocols/Shadowrocket.php
+++ app/Protocols/Shadowrocket.php
@@ -25,9 +25,10 @@ class Shadowrocket
         //display remaining traffic and expire date
         $upload = round($user['u'] / (1024*1024*1024), 2);
         $download = round($user['d'] / (1024*1024*1024), 2);
+        $ud = $upload + $download;
         $totalTraffic = round($user['transfer_enable'] / (1024*1024*1024), 2);
         $expiredDate = date('Y-m-d', $user['expired_at']);
-        $uri .= "STATUS=🚀↑:{$upload}GB,↓:{$download}GB,TOT:{$totalTraffic}GB💡Expires:{$expiredDate}\r\n";
+        $uri .= "STATUS=🚀已用流量:{$ud}GB,总流量:{$totalTraffic}GB💡到期时间:{$expiredDate}\r\n";
         foreach ($servers as $item) {
             if ($item['type'] === 'shadowsocks') {
                 $uri .= self::buildShadowsocks($user['uuid'], $item);
