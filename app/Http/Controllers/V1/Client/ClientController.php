c6dfd021ffcfdba3326a8c852d05c037c5b38bd7
adminv2pass <admin@v2pass.net>
2024-03-23 21:57:51 +0800

    fix

--- app/Http/Controllers/V1/Client/ClientController.php
+++ app/Http/Controllers/V1/Client/ClientController.php
@@ -227,7 +227,7 @@ class ClientController extends Controller
             } elseif (stripos($isp, 'CNIC-CAS') !== false) {
                 $translatedISP = "【科技网】";
             } else {
-                $translatedISP = "【未知】";
+                $translatedISP = "$isp";
             }
 
             $result = "{$country}{$region}{$city}{$translatedISP}";
