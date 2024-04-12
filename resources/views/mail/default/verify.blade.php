c534f91ab3ce7918bf0aac105e214d113f6b30e5
adminv2pass <admin@v2pass.net>
2024-04-12 15:10:48 +0800

    new email verify template

--- resources/views/mail/default/verify.blade.php
+++ resources/views/mail/default/verify.blade.php
@@ -1,7 +1,8 @@
 <div style="max-width: 600px; margin: 0 auto; padding: 20px; font-family: Arial, sans-serif;">
+    <img src="https://qstheorycctvxinhuanetchinaweeklyhuanqiu12306.top/pics/Cat-logo-transparent.svg" alt="Logo" style="display: block; margin: 0 auto; width: 150px;">
     <h2 style="text-align: center; color: #333;">{{$name}} 邮箱验证码</h2>
     <p style="text-align: center; color: #666; font-size: 16px;">请在{{$name}}上输入以下验证码：</p>
-    <div style="background-color: #4CAF50; color: white; text-align: center; font-size: 24px; padding: 10px; margin: 20px auto; width: 200px; border-radius: 5px;">
+    <div style="background-color: #46a2a0; color: white; text-align: center; font-size: 24px; padding: 10px; margin: 20px auto; width: 200px; border-radius: 5px;">
         {{$code}}
     </div>
     <p style="text-align: center; color: #666; font-size: 14px; margin-top: 20px;">请勿将此验证码透露给他人。</p>
