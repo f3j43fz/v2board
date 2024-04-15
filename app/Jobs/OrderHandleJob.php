91aff169c26b7ff50796720ded948053d1d7a14e
adminv2pass <admin@v2pass.net>
2024-04-15 11:30:47 +0800

    bug fix test

--- app/Jobs/OrderHandleJob.php
+++ app/Jobs/OrderHandleJob.php
@@ -13,20 +13,21 @@ use Illuminate\Queue\SerializesModels;
 class OrderHandleJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected $order;
 
-    protected $tradeNo;
     public $tries = 3;
     public $timeout = 5;
-
     /**
      * Create a new job instance.
      *
-     * @param string $tradeNo 交易编号
+     * @return void
      */
     public function __construct($tradeNo)
     {
         $this->onQueue('order_handle');
-        $this->tradeNo = $tradeNo;
+        $this->order = Order::where('trade_no', $tradeNo)
+            ->lockForUpdate()
+            ->first();
     }
 
     /**
@@ -36,45 +37,31 @@ class OrderHandleJob implements ShouldQueue
      */
     public function handle()
     {
-        // 从数据库中查询并锁定订单
-        $order = Order::where('trade_no', $this->tradeNo)
-            ->lockForUpdate()
-            ->first();
+        if (!$this->order) return;
+        $orderService = new OrderService($this->order);
+        switch ($this->order->status) {
+            // cancel
+            case 0:
+                if ($this->order->created_at <= (time() - 3600 * 2)) {
+                    $orderService->cancel();
+                }
+                break;
+            case 1:
 
-        if (!$order) {
-            return;
-        }
-
-        // 提前处理取消订单的逻辑
-        if ($order->status == 0 && $order->created_at <= (time() - 3600 * 2)) {
-            $orderService = new OrderService($order);
-            $orderService->cancel();
-            return;
-        }
-
-        $orderService = new OrderService($order);
-
-        if ($order->plan_id == 100) {
-            // 充值余额
-            $this->handleRecharge($order, $orderService);
-        } else {
-            // 正常购买套餐
-            $this->handlePurchase($order, $orderService);
+                if ($this->order->plan_id == 100) {
+                    // 充值余额
+                    $orderService->recharge();
+                } else {
+                    // 正常购买套餐
+                    $this->handlePurchase($this->order, $orderService);
+                }
+                break;
         }
     }
 
-    private function handleRecharge(Order $order, OrderService $orderService)
-    {
-        if ($order->status == 1) {
-            $orderService->recharge();
-        }
-    }
 
     private function handlePurchase(Order $order, OrderService $orderService)
     {
-        if ($order->status != 1) {
-            return;
-        }
 
         if ($order->period == "setup_price") {
             // 开通【随用随付】
@@ -87,4 +74,7 @@ class OrderHandleJob implements ShouldQueue
             $orderService->open();
         }
     }
+
+
+
 }
