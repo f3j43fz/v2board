3c0f31240bf932682345a34da4aa5b88b6d2b44e
adminv2pass <admin@v2pass.net>
2024-04-29 11:21:02 +0800

    recharge gift limit : 100

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -135,8 +135,8 @@ class OrderService
         // 路径：/config/v2board.php
         // 之后，记得修改管理员前端，方便后续修改
 
-        // 门槛： 200 元   不够200则没有优惠，即充多少是多少。
-        $discountThreshold = config('v2board.discount_threshold', 200 * 100);
+        // 门槛： 100 元   不够100则没有优惠，即充多少是多少。
+        $discountThreshold = config('v2board.discount_threshold', 100 * 100);
         // 优惠比例： 20%
         $discount = config('v2board.recharge_discount', 20) * 0.01;
 
