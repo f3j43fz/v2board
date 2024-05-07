41a81e1f1f47b4d2b9c86a710f902f98d867d852
adminv2pass <admin@v2pass.net>
2024-05-07 15:53:48 +0800

    test: more user friendly

--- app/Protocols/Clash.php
+++ app/Protocols/Clash.php
@@ -64,14 +64,20 @@ class Clash
                 array_push($proxies, $item['name']);
             }
 
-//            if ($item['type'] === 'vless') {
-//                array_push($proxy, self::buildVless($user['uuid'], $item));
-//                array_push($proxies, $item['name']);
-//            }
-
             if ($item['type'] === 'trojan') {
                 array_push($proxy, self::buildTrojan($user['uuid'], $item));
                 array_push($proxies, $item['name']);
+            }
+
+            if ($item['type'] === 'vless' || $item['type'] === 'hysteria') {
+                $info1 = '本客户端不支持直连套餐';
+                array_push($proxy, self::buildErrorInfo($info1));
+                array_push($proxies, $info1);
+
+                $info2 = '请下载其他客户端';
+                array_push($proxy, self::buildErrorInfo($info2));
+                array_push($proxies, $info2);
+
             }
         }
 
@@ -178,69 +184,6 @@ class Clash
         return $array;
     }
 
-    public static function buildVless($uuid, $server)
-    {
-        $array = [];
-        $array['name'] = $server['name'];
-        $array['type'] = 'vless';
-        $array['server'] = $server['host'];
-        $array['port'] = $server['port'];
-        $array['uuid'] = $uuid;
-        $array['udp'] = true;
-        $array['xudp'] = true;
-        $array['flow'] = !empty($server['flow']) ? $server['flow']: "";
-        if ($server['tls']) {
-            $array['tls'] = true;
-            if ($server['tls_settings']) {
-                $tls_settings = $server['tls_settings'];
-                if (!empty($tls_settings['allowInsecure']))
-                    $array['skip-cert-verify'] = (bool)$tls_settings['allowInsecure'];
-                if (!empty($tls_settings['server_name']))
-                    $array['servername'] = $tls_settings['server_name'];
-                if ((int)$server['tls'] === 2) {
-                    $publicKey = $tls_settings['public_key'];
-                    $shortID = $tls_settings['short_id'];
-                    $array['reality-opts'] = [
-                        'public-key' => $publicKey,
-                        'short-id' => $shortID
-                    ];
-                    $array['client-fingerprint'] = 'chrome';
-                }
-            }
-        }
-        if ($server['network'] === 'tcp') {
-            $tcpSettings = $server['network_settings'];
-            if (isset($tcpSettings['header']['type'])) $array['network'] = $tcpSettings['header']['type'];
-            if (isset($tcpSettings['header']['request']['path'][0])) $array['http-opts']['path'] = $tcpSettings['header']['request']['path'][0];
-        }
-        if ($server['network'] === 'ws') {
-            $array['network'] = 'ws';
-            if ($server['network_settings']) {
-                $wsSettings = $server['network_settings'];
-                $array['ws-opts'] = [];
-                if (isset($wsSettings['path']) && !empty($wsSettings['path']))
-                    $array['ws-opts']['path'] = $wsSettings['path'];
-                if (isset($wsSettings['headers']['Host']) && !empty($wsSettings['headers']['Host']))
-                    $array['ws-opts']['headers'] = ['Host' => $wsSettings['headers']['Host']];
-                if (isset($wsSettings['path']) && !empty($wsSettings['path']))
-                    $array['ws-path'] = $wsSettings['path'];
-                if (isset($wsSettings['headers']['Host']) && !empty($wsSettings['headers']['Host']))
-                    $array['ws-headers'] = ['Host' => $wsSettings['headers']['Host']];
-            }
-        }
-        if ($server['network'] === 'grpc') {
-            $array['network'] = 'grpc';
-            if ($server['network_settings']) {
-                $grpcSettings = $server['network_settings'];
-                $array['grpc-opts'] = [];
-                if (isset($grpcSettings['service_name'])) {
-                    $array['grpc-opts'] = ["grpc-service-name" => $grpcSettings['service_name']];
-                }
-            }
-        }
-        return $array;
-    }
-
     public static function buildTrojan($password, $server)
     {
         $array = [];
@@ -254,6 +197,22 @@ class Clash
         if (!empty($server['allow_insecure'])) $array['skip-cert-verify'] = ($server['allow_insecure'] ? true : false);
         return $array;
     }
+
+
+    private static function buildErrorInfo($info)
+    {
+        $array = [];
+        $array['name'] = $info;
+        $array['type'] = 'ss';
+        $array['server'] = 'baidu.com';
+        $array['port'] = 8080;
+        $array['cipher'] = 'aes-256-gcm';
+        $array['password'] = 'password';
+        $array['udp'] = true;
+        return $array;
+
+    }
+
 
     private function isMatch($exp, $str)
     {
