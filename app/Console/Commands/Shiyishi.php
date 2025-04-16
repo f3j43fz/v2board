4a8cf0e2eecd9631b441f684617fce243e3114f4
bala <fdsfdsf@sina.com>
2025-04-16 16:06:15 +0800

    update ipv4

--- app/Console/Commands/Shiyishi.php
+++ app/Console/Commands/Shiyishi.php
@@ -3,7 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-
+use ipip\db\City;
 class Shiyishi extends Command
 {
     /**
@@ -38,12 +38,9 @@ class Shiyishi extends Command
     public function handle()
     {
 
-        // 离线 IP 数据库路径（必须和下载命令保存的路径一致）
         $ipdbPath = resource_path('ipdata/qqwry.ipdb');
-        // 全球 IPv4 地级市精度离线库（China：免费版，每周高级版，每日标准版，每日高级版，每日专业版，每日旗舰版）
-        // 使用 ipdb-php 库进行离线查询
-        $city = new \src\ipip\db\City($ipdbPath);
-        // 使用第二个参数 'CN' 指定中文格式（如有需要，可根据官方说明调整）
+        // 使用 use 导入后的类名，直接 new
+        $city = new City($ipdbPath);
         var_dump($city->find('118.28.1.1', 'CN'));
     }
 }
