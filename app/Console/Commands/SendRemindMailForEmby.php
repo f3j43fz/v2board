3e6f0abf349b7bc0cdca87301fbee4c1ab105ee8
bala <fdsfdsf@sina.com>
2025-05-01 20:49:58 +0800

    配置更新 增加删除emby用户功能

---
+++ app/Console/Commands/SendRemindMailForEmby.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\MailService;
+use Illuminate\Console\Command;
+use App\Models\User;
+use App\Models\MailLog;
+use App\Jobs\SendEmailJob;
+
+class SendRemindMailForEmby extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:remindMailForEmbyUsers';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '发送提醒邮件';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        ini_set('memory_limit', -1);
+        $users = User::all();
+        $mailService = new MailService();
+        foreach ($users as $user) {
+            if ($user->banned) continue;
+            if ($user->remind_expire) {
+                $mailService->remindEmbyExpire($user);
+            }
+        }
+    }
+}
