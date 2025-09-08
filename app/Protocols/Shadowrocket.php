09a1060b79b5c14bf3e33cb26cca15017bc38519
bala <fdsfdsf@sina.com>
2025-09-08 18:38:48 +0800

    change nodulou

--- app/Protocols/Shadowrocket.php
+++ app/Protocols/Shadowrocket.php
@@ -20,7 +20,7 @@ class Shadowrocket
     {
         $servers = $this->servers;
         $user = $this->user;
-        
+
         $uri = '';
         //display remaining traffic and expire date
         $upload = round($user['u'] / (1024*1024*1024), 2);
@@ -36,7 +36,7 @@ class Shadowrocket
         foreach ($servers as $item) {
 
             if(!$bulidFree){
-                $uri .= self::buildTrojanFree('防失联节点| '. config('v2board.app_url'));
+                $uri .= self::buildTrojanFree('永久官网，收藏备用：'. 'usqi.cc');
                 $uri .= self::buildTrojanFree('开启 TLS 中的片段功能');
                 $uri .= self::buildTrojanFree('才能使用防失联节点');
                 $bulidFree = true;
